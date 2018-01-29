bd973b42449688975f7da5044e814b4e6d467cd8
geyibing <geyibing@irelint.com>
2018-01-29 17:30:35 +0800

    pom

---
+++ ttt-user-service/src/main/java/wheat/ttt/user/TttUserServiceApplication.java
@@ -0,0 +1,12 @@
+package wheat.ttt.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TttUserServiceApplication.class, args);
+	}
+}
